e5a15f32f2c7357c83b45616c8bcdb4364afcf1e
Castro34f <yanechcastro@gmail.com>
2025-10-08 14:03:42 +0100

    Update CSS imports and Tailwind setup

--- main.tsx
+++ main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Toaster } from "sonner";
 import App from "./src/App";  // <-- Correct relative path
-import "./src/styles/globals.css";
+import './src/index.css';
+
 
 const rootElement = document.getElementById("root");
 
